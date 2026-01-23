7da9118880e2970d614b6661f7230edfa5ed8af4
Claudio-Arz <claudio_arz@yahoo.com>
2026-01-23 00:33:40 -0300

    modified:   HTML/CSS/mainHTML.css 	modified:   HTML/RPM.html 	modified:   HTML/variometer.html 	modified:   actualizar_html_github.bat

--- actualizar_html_github.bat
+++ actualizar_html_github.bat
@@ -9,11 +9,29 @@ git add .
 
 REM Crea un commit con mensaje automático y fecha/hora
 set FECHA=%DATE% %TIME%
-git commit -m "Actualización automática %FECHA%"
+git commit -m "Actualizacion automatica %FECHA%"
 
 REM Sube los cambios a la rama main
 git push origin main
 
 REM Mensaje final
 echo HTML actualizado en GitHub Pages: https://claudio-arz.github.io/AeroDeck-HTML/
+echo Esto sube los archivos que GitHub Pages utiliza para mostrar el sitio web.
+REM Script para actualizar el repositorio Claudio-Arz/AeroDeck_001
+
+REM Cambia al directorio del repositorio CockPit (ajusta la ruta si es necesario)
+cd /d "C:\ESPLab\ZZZTmpTests\LuisSystems\Instrumentos_Calibración_Versión002\AeroDeck_001"
+
+REM Agrega todos los cambios
+git add .
+
+REM Crea un commit con mensaje automático y fecha/hora
+set FECHA=%DATE% %TIME%
+git commit -m "Actualización automática %FECHA%"
+
+REM Sube los cambios a la rama main
+git push origin main
+
+REM Mensaje final
+echo CockPit actualizado en GitHub: https://github.com/Claudio-Arz/CockPit
 pause
