e182dbc1c3c9b4e3c9d6e08be4bbda304fc448f7
Claudio-Arz <claudio_arz@yahoo.com>
2026-01-23 04:31:32 -0300

    Actualizacion automatica Fri 01/23/2026  4:31:32.61

---
+++ actualizar_html_github.bat
@@ -0,0 +1,24 @@
+
+REM Script para actualizar la carpeta HTML en el repositorio público AeroDeck-HTML
+
+REM Cambia al directorio de la carpeta HTML
+
+
+git add .
+set FECHA=%DATE% %TIME%
+git commit -m "Actualizacion automatica %FECHA%"
+git push origin main
+
+
+
+REM Mensaje final
+echo HTML actualizado en GitHub Pages: https://claudio-arz.github.io/AeroDeck-HTML/
+echo Esto sube los archivos que GitHub Pages utiliza para mostrar el sitio web.
+
+pause
+rem git add RPM.html variometer.html
+rem git commit -m "Rescate de cambios desde HEAD detached"
+rem git checkout main
+rem git checkout main
+rem git merge rescate_trabajo
+rem git push origin main
