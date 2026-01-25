06ca99a07ea1e0808d2973e4e8dde9abfdbe3342
Claudio-Arz <claudio_arz@yahoo.com>
2026-01-25 04:26:18 -0300

    Actualizaci├│n autom├ítica

--- HTML/mainHTML.cpp
+++ HTML/mainHTML.cpp
@@ -42,6 +42,57 @@ const char MAIN_page[] PROGMEM = R"rawliteral(
 <script>
 
 
+</script>
+</head>
+
+<body>
+
+<h1 style="text-align:center; margin-top: 24px;">Banco de Prueba y Calibración</h1>
+
+
+<div id="main-grid" class="grid-container">
+  
+  <div class="grid-item" id="inst01" style="grid-row: 1; grid-column: 1;"></div>  
+  <div class="grid-item" id="inst02" style="grid-row: 1; grid-column: 2;"></div>  
+  <div class="grid-item" id="inst03" style="grid-row: 1; grid-column: 3;"></div>  
+  <div class="grid-item" id="inst04" style="grid-row: 1; grid-column: 4;"></div>  
+  <div class="grid-item" id="inst05" style="grid-row: 1; grid-column: 5;"></div>  
+  <div class="grid-item" id="inst06" style="grid-row: 1; grid-column: 6;"></div>  
+  <div class="grid-item" id="inst07" style="grid-row: 1; grid-column: 7;"></div>  
+  <div class="grid-item" id="inst08" style="grid-row: 1; grid-column: 8;"></div>  
+  <div class="grid-item" id="inst09" style="grid-row: 2; grid-column: 1;"></div>  
+  <div class="grid-item" id="inst10" style="grid-row: 2; grid-column: 2;"></div>  
+  <div class="grid-item" id="inst11" style="grid-row: 2; grid-column: 3;"></div>  
+  <div class="grid-item" id="inst12" style="grid-row: 2; grid-column: 4;"></div>  
+  <div class="grid-item" id="inst13" style="grid-row: 2; grid-column: 5;"></div>  
+  <div class="grid-item" id="inst14" style="grid-row: 2; grid-column: 6;"></div>  
+  <div class="grid-item" id="inst15" style="grid-row: 2; grid-column: 7;"></div>  
+  <div class="grid-item" id="inst16" style="grid-row: 2; grid-column: 8;"></div>
+
+</div>
+
+<!-- El contenido HTML principal se cargará aquí dinámicamente -->
+</div>
+</body>
+
+<script>
+// Inicialización del WebSocket para comunicación con el ESP32
+const ws = new WebSocket('ws://' + location.hostname + ':81/');
+
+// === Funciones de actualización de instrumentos ===
+// Cargar el HTML principal de forma dinámica
+window.addEventListener('DOMContentLoaded', () => {
+  fetch("https://claudio-arz.github.io/AeroDeck-HTML/mainHTML.html")
+  .then(r => r.text())
+  .then(html => {
+    document.getElementById("main-html").innerHTML = html;
+    if (typeof setupVariometerControls === 'function') {
+      setupVariometerControls(ws);
+    }
+  });
+});
+
+
 // Inicialización del WebSocket para comunicación con el ESP32
 const ws = new WebSocket('ws://' + location.hostname + ':81/');
 
@@ -98,14 +149,6 @@ ws.onmessage = (msg) => {
 
 };
 
-// Cargar el HTML del instrumento RPM de forma dinámica
-window.addEventListener('DOMContentLoaded', () => {
-  fetch("https://claudio-arz.github.io/AeroDeck-HTML/RPM.html")
-    .then(r => r.text())
-    .then(html => {
-      document.getElementById("inst04").innerHTML = html;
-    });
-});
 // Cargar el HTML del Controles de RPM de forma dinámica
 window.addEventListener('DOMContentLoaded', () => {
   fetch("https://claudio-arz.github.io/AeroDeck-HTML/RPM_Control.html")
@@ -135,34 +178,7 @@ window.addEventListener('DOMContentLoaded', () => {
         setupVariometerControls(ws);
       }
     });
-});
-
-</script>
-</head>
-
-<body>
-<div id="main-html">
-<!-- El contenido HTML principal se cargará aquí dinámicamente -->
-</div>
-</body>
-
-<script>
-// Inicialización del WebSocket para comunicación con el ESP32
-const ws = new WebSocket('ws://' + location.hostname + ':81/');
-
-// === Funciones de actualización de instrumentos ===
-// Cargar el HTML principal de forma dinámica
-window.addEventListener('DOMContentLoaded', () => {
-  fetch("https://claudio-arz.github.io/AeroDeck-HTML/mainHTML.html")
-  .then(r => r.text())
-  .then(html => {
-    document.getElementById("main-html").innerHTML = html;
-    if (typeof setupVariometerControls === 'function') {
-      setupVariometerControls(ws);
-    }
-  });
-});
-      
+});      
 </script>
 </html>
 )rawliteral";
