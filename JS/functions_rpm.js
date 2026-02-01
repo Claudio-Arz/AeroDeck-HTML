01327796c8b1f7a405a023c8b6e6e9ef60fe7a66
Claudio-Arz <claudio_arz@yahoo.com>
2026-01-31 21:59:17 -0300

    Incorporar AirSpeed (instrumento y control deslizante). Nueva librería JS. [Sat 01/31/2026 21:59:16.96]

--- JS/functions_rpm.js
+++ JS/functions_rpm.js
@@ -44,7 +44,7 @@ function animateNeedleRPM(newValue) {
   if (!rpmAnimationFrame) {
     function step() {
       currentRPM += (targetRPM - currentRPM) * 1; // valor original 0.2, valor propuesto por copilot o.5 para más rapidez
-      if (Math.abs(targetRPM - currentRPM) < 0.5) {
+      if (Math.abs(targetRPM - currentRPM) < 1) {
         currentRPM = targetRPM;
         rpmAnimationFrame = null;
       } else {
