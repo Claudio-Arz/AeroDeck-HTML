06261250ac7fd50e377e5e440fa2ac75139c2860
Claudio-Arz <claudio_arz@yahoo.com>
2026-01-31 22:27:50 -0300

    Incorporar AirSpeed (instrumento y control deslizante). Nueva librería JS. [Sat 01/31/2026 22:27:50.21]

--- JS/functions_airSpeed.js
+++ JS/functions_airSpeed.js
@@ -22,8 +22,8 @@ const AirSpeed = (function() {
       const val = (typeof airspeed === 'number') ? airspeed : parseFloat(sliders.valor.value);
       // Limitar el valor al rango real
       const safeVal = Math.max(min, Math.min(val, max));
-      // Mapea 40-200 nudos a 30° (mínimo) a 323° (máximo) (giro horario)
-      let angle = 28 + ((safeVal - min) * (323 - 28)) / (max - min);
+      // Mapea 40-200 nudos a 30° (mínimo) a 320° (máximo) (giro horario)
+      let angle = 28 + ((safeVal - min) * (320 - 28)) / (max - min);
       imgs.aguja.style.transform = `rotate(${angle}deg)`;
       // Actualiza valor numérico si existe
       const valueEl = getEl('as-value');
