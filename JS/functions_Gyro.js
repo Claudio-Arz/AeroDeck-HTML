98b9253279f4454aa6747c288a0f0ceac90ce911
Claudio-Arz <claudio_arz@yahoo.com>
2026-02-07 09:26:50 -0300

    Gyro funciona. Hay que actualizar otras terminales. El valor salta en todas las terminales [Sat 02/07/2026  9:26:50.30]

--- JS/functions_Gyro.js
+++ JS/functions_Gyro.js
@@ -15,7 +15,7 @@ function animateDialGyro(newValue) {
   targetGyro = newValue;
   if (!gyroAnimationFrame) {
     function step() {
-      currentGyro += (targetGyro - currentGyro) * 0.2;
+      currentGyro += (targetGyro - currentGyro) * 1; // 0.2
       if (Math.abs(targetGyro - currentGyro) < 0.5) {
         currentGyro = targetGyro;
         gyroAnimationFrame = null;
