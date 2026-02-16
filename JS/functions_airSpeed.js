47c8e57c95863839a2938c2d7c50f09ccb362998
Claudio-Arz <claudio_arz@yahoo.com>
2026-02-16 03:01:14 -0300

    Realmente los últimos ajustes para Air Speed, y paso a otro instrumento [Mon-02-16  3:01:14.39]

--- JS/functions_airSpeed.js
+++ JS/functions_airSpeed.js
@@ -107,11 +107,11 @@ function sendAirspeedToESP32(airspeed) {
     console.warn('WebSocket no está conectado.');
   }
 }
-// Mapea el valor de AirSpeed (40-200) al ángulo de la aguja (32-324)
+// Mapea el valor de AirSpeed (40-200) al ángulo de la aguja (34-324)
 function airspeedToAngle(airspeed) {
   const minValue = 40;
   const maxValue = 200;
-  const minAngle = 32;
+  const minAngle = 34;
   const maxAngle = 324;
   if (airspeed < minValue) airspeed = minValue;
   if (airspeed > maxValue) airspeed = maxValue;
