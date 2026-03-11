6e7ee7d599b3642d448c67d5bd5b7250c044d733
Claudio-Arz <claudio_arz@yahoo.com>
2026-03-11 01:19:30 -0300

    Ajustando skid-skit ball

--- mainHTML.cpp
+++ mainHTML.cpp
@@ -173,9 +173,9 @@ window.addEventListener('DOMContentLoaded', () => {
       // console.log("Actualizando TC Avión: " + data['tc-rollValue']);
       updateTurnCoordinatorPlane(data['tc-rollValue']);
     }
-    if (data['tc-pitchValue'] !== undefined && typeof updateTurnCoordinatorBall === 'function') {
-      // console.log("Actualizando TC Péndulo: " + data['tc-pitchValue']);
-      updateTurnCoordinatorBall(data['tc-pitchValue']);
+    if (data.tcBallValue !== undefined && typeof updateTurnCoordinatorBall === 'function') {
+      // console.log("Actualizando TC Péndulo: " + data.tcBallValue);
+      updateTurnCoordinatorBall(data.tcBallValue);
     }
     if (data['fuelFlowValue'] !== undefined && typeof updateFuelFlow === 'function') {
       // console.log("Actualizando Fuel Flow: " + data['fuelFlowValue']);
